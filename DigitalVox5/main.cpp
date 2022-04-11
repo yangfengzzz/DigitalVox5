cec2565473ba63c5ff920f006ef8925316f08aba
yangfengzzz <yangfengzzz@hotmail.com>
2022-04-11 08:10:15 +0800

    Initial Commit

---
+++ DigitalVox5/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DigitalVox5
+//
+//  Created by 杨丰 on 2022/4/11.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
