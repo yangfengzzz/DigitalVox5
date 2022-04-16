85a2d5a7f4f46a509678d6ab6a9d2bfe6ef76ef1
yangfengzzz <yangfengzzz@hotmail.com>
2022-04-16 21:52:24 +0800

    add mesh

---
+++ vox.render/mesh/mesh.cpp
@@ -0,0 +1,71 @@
+//  Copyright (c) 2022 Feng Yang
+//
+//  I am making my contributions/submissions to this project solely in my
+//  personal capacity and am not conveying any rights to any intellectual
+//  property of any third parties.
+
+#include "mesh.h"
+
+namespace vox {
+uint32_t Mesh::instance_count() const {
+    return instance_count_;
+}
+
+const SubMesh *Mesh::sub_mesh() const {
+    if (!sub_meshes_.empty()) {
+        return &sub_meshes_[0];
+    }
+    return nullptr;
+}
+
+const std::vector<SubMesh> &Mesh::sub_meshes() const {
+    return sub_meshes_;
+}
+
+void Mesh::add_sub_mesh(SubMesh sub_mesh) {
+    sub_meshes_.push_back(sub_mesh);
+}
+
+void Mesh::add_sub_mesh(uint32_t start, uint32_t count) {
+    sub_meshes_.emplace_back(start, count);
+}
+
+void Mesh::clear_sub_mesh() {
+    sub_meshes_.clear();
+}
+
+std::unique_ptr<UpdateFlag> Mesh::register_update_flag() {
+    return update_flag_manager_.registration();
+}
+
+void Mesh::set_vertex_input_state(const VkPipelineVertexInputStateCreateInfo &layouts) {
+    vertex_input_state_ = initializers::pipeline_vertex_input_state_create_info();
+    vertex_input_state_ = layouts;
+    update_flag_manager_.distribute();
+}
+
+void Mesh::set_vertex_buffer_binding(size_t index, std::unique_ptr<core::Buffer> &&binding) {
+    if (vertex_buffer_bindings_.size() > index) {
+        vertex_buffer_bindings_[index] = std::move(binding);
+    } else {
+        vertex_buffer_bindings_.insert(vertex_buffer_bindings_.begin() + index, std::move(binding));
+    }
+}
+
+void Mesh::set_index_buffer_binding(std::unique_ptr<IndexBufferBinding> &&binding) {
+    index_buffer_binding_ = std::move(binding);
+}
+
+const VkPipelineVertexInputStateCreateInfo &Mesh::vertex_input_state() const {
+    return vertex_input_state_;
+}
+
+const std::vector<std::unique_ptr<core::Buffer>> &Mesh::vertex_buffer_bindings() const {
+    return vertex_buffer_bindings_;
+}
+
+const IndexBufferBinding &Mesh::index_buffer_binding() const {
+    return *index_buffer_binding_;
+}
+
+}
---
+++ vox.render/mesh/mesh.h
@@ -0,0 +1,95 @@
+//  Copyright (c) 2022 Feng Yang
+//
+//  I am making my contributions/submissions to this project solely in my
+//  personal capacity and am not conveying any rights to any intellectual
+//  property of any third parties.
+
+#ifndef DIGITALVOX_VOX_RENDER_MESH_MESH_H_
+#define DIGITALVOX_VOX_RENDER_MESH_MESH_H_
+
+#include "sub_mesh.h"
+#include "index_buffer_binding.h"
+#include "bounding_box3.h"
+#include "update_flag_manager.h"
+#include "../vk_initializers.h"
+#include <string>
+
+namespace vox {
+class Mesh {
+public:
+    /** Name. */
+    std::string name_;
+    /** The bounding volume of the mesh. */
+    BoundingBox3F bounds_ = BoundingBox3F();
+    
+    /**
+     * Instanced count, disable instanced drawing when set zero.
+     */
+    [[nodiscard]] uint32_t instance_count() const;
+    
+    /**
+     * First sub-mesh. Rendered using the first material.
+     */
+    [[nodiscard]] const SubMesh *sub_mesh() const;
+    
+    /**
+     * A collection of sub-mesh, each sub-mesh can be rendered with an independent material.
+     */
+    [[nodiscard]] const std::vector<SubMesh> &sub_meshes() const;
+    
+    /**
+     * Add sub-mesh, each sub-mesh can correspond to an independent material.
+     * @param sub_mesh - Start drawing offset, if the index buffer is set,
+     * it means the offset in the index buffer, if not set, it means the offset in the vertex buffer
+     */
+    void add_sub_mesh(SubMesh sub_mesh);
+    
+    /**
+     * Add sub-mesh, each sub-mesh can correspond to an independent material.
+     * @param start - Start drawing offset, if the index buffer is set,
+     * it means the offset in the index buffer, if not set,
+     * it means the offset in the vertex buffer
+     * @param count - Drawing count, if the index buffer is set,
+     * it means the count in the index buffer, if not set,
+     * it means the count in the vertex buffer
+     */
+    void add_sub_mesh(uint32_t start = 0, uint32_t count = 0);
+    
+    /**
+     * Clear all sub-mesh.
+     */
+    void clear_sub_mesh();
+    
+    /**
+     * Register update flag, update flag will be true if the vertex element changes.
+     * @returns Update flag
+     */
+    std::unique_ptr<UpdateFlag> register_update_flag();
+    
+public:
+    [[nodiscard]] const VkPipelineVertexInputStateCreateInfo &vertex_input_state() const;
+    
+    [[nodiscard]] const std::vector<std::unique_ptr<core::Buffer>> &vertex_buffer_bindings() const;
+    
+    [[nodiscard]] const IndexBufferBinding &index_buffer_binding() const;
+    
+protected:
+    void set_vertex_input_state(const VkPipelineVertexInputStateCreateInfo &layouts);
+    
+    void set_vertex_buffer_binding(size_t index, std::unique_ptr<core::Buffer> &&binding);
+    
+    void set_index_buffer_binding(std::unique_ptr<IndexBufferBinding> &&binding);
+    
+protected:
+    uint32_t instance_count_ = 1;
+    std::vector<std::unique_ptr<core::Buffer>> vertex_buffer_bindings_{};
+    std::unique_ptr<IndexBufferBinding> index_buffer_binding_{nullptr};
+    VkPipelineVertexInputStateCreateInfo vertex_input_state_;
+    
+    std::vector<SubMesh> sub_meshes_{};
+    UpdateFlagManager update_flag_manager_;
+};
+
+}
+
+#endif /* DIGITALVOX_VOX_RENDER_MESH_MESH_H_ */
