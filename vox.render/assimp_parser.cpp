a932449b507b9cafb1ba4721f03d75d0a8ab4827
yangfengzzz <yangfengzzz@hotmail.com>
2022-04-22 15:15:58 +0800

    assimp parser as asset main entry

---
+++ vox.render/assimp_parser.cpp
@@ -0,0 +1,225 @@
+//  Copyright (c) 2022 Feng Yang
+//
+//  I am making my contributions/submissions to this project solely in my
+//  personal capacity and am not conveying any rights to any intellectual
+//  property of any third parties.
+
+#include "assimp_parser.h"
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/matrix4x4.h>
+#include <assimp/postprocess.h>
+#include "logging.h"
+#include "entity.h"
+#include "mesh/model_mesh.h"
+#include "mesh/mesh_renderer.h"
+#include "material/unlit_material.h"
+#include "material/blinn_phong_material.h"
+#include "material/pbr_material.h"
+#include "material/pbr_specular_material.h"
+
+namespace vox {
+AssimpParser::AssimpParser(Device& device):
+device_(device) {
+}
+
+void AssimpParser::load_model(Entity* root, const std::string& file, unsigned int pFlags) {
+    Assimp::Importer importer;
+    const aiScene* scene = importer.ReadFile(file, pFlags | aiProcess_CalcTangentSpace);
+    // check for errors
+    if(!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode) {
+        LOGE("ERROR::ASSIMP:: %s", importer.GetErrorString())
+    }
+    
+    // process ASSIMP's root node recursively
+    process_node(root, scene->mRootNode, scene);
+}
+
+void AssimpParser::process_node(Entity* root, aiNode *node, const aiScene *scene) {
+    auto entity = root->create_child();
+    entity->name_ = node->mName.C_Str();
+    auto& transform = node->mTransformation;
+    entity->transform_->set_local_matrix(Matrix4x4F(transform.a1, transform.b1, transform.c1, transform.d1,
+                                                    transform.a2, transform.b2, transform.c2, transform.d2,
+                                                    transform.a3, transform.b3, transform.c3, transform.d3,
+                                                    transform.a4, transform.b4, transform.c4, transform.d4));
+    
+    // process each mesh located at the current node
+    for(unsigned int i = 0; i < node->mNumMeshes; i++) {
+        // the node object only contains indices to index the actual objects in the scene.
+        // the scene contains all the data, node is just to keep stuff organized (like relations between nodes).
+        process_mesh(entity, scene->mMeshes[node->mMeshes[i]], scene);
+    }
+    
+    // after we've processed all of the meshes (if any) we then recursively process each of the children nodes
+    for(unsigned int i = 0; i < node->mNumChildren; i++) {
+        process_node(entity, node->mChildren[i], scene);
+    }
+}
+
+void AssimpParser::process_mesh(Entity* root, aiMesh *mesh, const aiScene *scene) {
+    auto renderer = root->add_component<MeshRenderer>();
+    auto model_mesh = std::make_shared<ModelMesh>(device_);
+    renderer->set_mesh(model_mesh);
+    
+    std::vector<Vector3F> vec3_array(mesh->mNumVertices);
+    for(unsigned int i = 0; i < mesh->mNumVertices; i++) {
+        vec3_array[i].x = mesh->mVertices[i].x;
+        vec3_array[i].y = mesh->mVertices[i].y;
+        vec3_array[i].z = mesh->mVertices[i].z;
+    }
+    model_mesh->set_positions(vec3_array);
+    
+    if (mesh->HasNormals()) {
+        for(unsigned int i = 0; i < mesh->mNumVertices; i++) {
+            vec3_array[i].x = mesh->mNormals[i].x;
+            vec3_array[i].y = mesh->mNormals[i].y;
+            vec3_array[i].z = mesh->mNormals[i].z;
+        }
+        model_mesh->set_normals(vec3_array);
+    }
+    
+    if (mesh->mTextureCoords[0]) {
+        // a vertex can contain up to 8 different texture coordinates. We thus make the assumption that we won't
+        // use models where a vertex can have multiple texture coordinates so we always take the first set (0).
+        std::vector<Vector2F> vec2_array(mesh->mNumVertices);
+        for(unsigned int i = 0; i < mesh->mNumVertices; i++) {
+            vec2_array[i].x = mesh->mTextureCoords[0]->x;
+            vec2_array[i].y = mesh->mTextureCoords[0]->y;
+        }
+        model_mesh->set_uvs(vec2_array);
+        
+        for(unsigned int i = 0; i < mesh->mNumVertices; i++) {
+            vec3_array[i].x = mesh->mTangents[i].x;
+            vec3_array[i].y = mesh->mTangents[i].y;
+            vec3_array[i].z = mesh->mTangents[i].z;
+        }
+        // todo
+        
+        for(unsigned int i = 0; i < mesh->mNumVertices; i++) {
+            vec3_array[i].x = mesh->mBitangents[i].x;
+            vec3_array[i].y = mesh->mBitangents[i].y;
+            vec3_array[i].z = mesh->mBitangents[i].z;
+        }
+        // todo
+    }
+    
+    if (mesh->mColors[0]) {
+        std::vector<Color> color_array(mesh->mNumVertices);
+        for(unsigned int i = 0; i < mesh->mNumVertices; i++) {
+            color_array[i].r = mesh->mColors[i]->r;
+            color_array[i].g = mesh->mColors[i]->g;
+            color_array[i].b = mesh->mColors[i]->b;
+            color_array[i].a = mesh->mColors[i]->a;
+        }
+        model_mesh->set_colors(color_array);
+    }
+    
+    std::vector<uint32_t> indices;
+    // now wak through each of the mesh's faces (a face is a mesh its triangle) and retrieve the corresponding vertex indices.
+    for(unsigned int i = 0; i < mesh->mNumFaces; i++) {
+        aiFace face = mesh->mFaces[i];
+        // retrieve all indices of the face and store them in the indices vector
+        for(unsigned int j = 0; j < face.mNumIndices; j++)
+            indices.push_back(face.mIndices[j]);
+    }
+    model_mesh->set_indices(indices);
+    model_mesh->add_sub_mesh(0, static_cast<uint32_t>(indices.size()));
+    model_mesh->upload_data(true);
+    
+    // process materials
+    renderer->set_material(process_material(scene->mMaterials[mesh->mMaterialIndex]));
+}
+
+std::shared_ptr<Material> AssimpParser::process_material(aiMaterial* material) {
+    std::shared_ptr<Material> result{nullptr};
+    aiShadingMode mode;
+    material->Get(AI_MATKEY_SHADING_MODEL, mode);
+    switch (mode) {
+        case aiShadingMode_Unlit: {
+            auto mat = std::make_shared<UnlitMaterial>(device_);
+            result = mat;
+            break;
+        }
+            
+        case aiShadingMode_Blinn:
+        case aiShadingMode_Phong : {
+            auto mat = std::make_shared<BlinnPhongMaterial>(device_);
+            result = mat;
+            break;
+        }
+            
+        case aiShadingMode_PBR_BRDF : {
+            float factor = -1.0;
+            material->Get(AI_MATKEY_ROUGHNESS_FACTOR, factor);
+            if (factor < 0) {
+                auto mat = std::make_shared<PbrSpecularMaterial>(device_);
+                result = mat;
+            } else {
+                auto mat = std::make_shared<PbrMaterial>(device_);
+                result = mat;
+            }
+            break;
+        }
+            
+        default:
+            LOGI("Unknown material type: %s", to_string(mode));
+            break;
+    }
+    
+    return result;
+}
+
+std::string AssimpParser::to_string(aiShadingMode mode) {
+    switch (mode) {
+        case aiShadingMode_Flat:
+            return "Flat shading.";
+            break;
+            
+        case aiShadingMode_Gouraud:
+            return "Simple Gouraud shading.";
+            break;
+            
+        case aiShadingMode_Phong:
+            return "Phong shading.";
+            break;
+            
+        case aiShadingMode_Blinn:
+            return "Phong-Blinn shading.";
+            break;
+            
+        case aiShadingMode_Toon:
+            return "Toon shading.";
+            break;
+            
+        case aiShadingMode_OrenNayar:
+            return "OrenNayar shading.";
+            break;
+            
+        case aiShadingMode_Minnaert:
+            return "Minnaert shading.";
+            break;
+            
+        case aiShadingMode_CookTorrance:
+            return "CookTorrance shading.";
+            break;
+
+        case aiShadingMode_Unlit:
+            return "Unlit shading.";
+            break;
+            
+        case aiShadingMode_Fresnel:
+            return "Fresnel shading.";
+            break;
+            
+        case aiShadingMode_PBR_BRDF:
+            return "Physically-Based Rendering (PBR) shading.";
+            break;
+            
+        default:
+            return "Shading Limit.";
+            break;
+    }
+}
+
+}
---
+++ vox.render/assimp_parser.h
@@ -0,0 +1,41 @@
+//  Copyright (c) 2022 Feng Yang
+//
+//  I am making my contributions/submissions to this project solely in my
+//  personal capacity and am not conveying any rights to any intellectual
+//  property of any third parties.
+
+#pragma once
+
+#include <string>
+#include <assimp/scene.h>
+#include "scene_forward.h"
+#include "core/device.h"
+
+namespace vox {
+class AssimpParser {
+public:
+    AssimpParser(Device& device);
+    
+    /**
+     * Simply load meshes from a file using assimp
+     * Return true on success
+     */
+    void load_model(Entity* root, const std::string& file, unsigned int pFlags);
+    
+    /**
+     * Processes a node in a recursive fashion.
+     * Processes each individual mesh located at the node and repeats this process on its children nodes (if any).
+     */
+    void process_node(Entity* root, aiNode *node, const aiScene *scene);
+    
+    void process_mesh(Entity* root, aiMesh *mesh, const aiScene *scene);
+    
+    std::shared_ptr<Material> process_material(aiMaterial* material);
+    
+private:
+    std::string to_string(aiShadingMode mode);
+    
+    Device& device_;
+};
+
+}
