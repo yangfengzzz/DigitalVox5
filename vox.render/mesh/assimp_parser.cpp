73626fe83965c787e03723135657e1e9e75fe6e8
yangfengzzz <yangfengzzz@hotmail.com>
2022-04-22 12:00:54 +0800

    add AssimpParser link assimp

---
+++ vox.render/mesh/assimp_parser.cpp
@@ -0,0 +1,20 @@
+//  Copyright (c) 2022 Feng Yang
+//
+//  I am making my contributions/submissions to this project solely in my
+//  personal capacity and am not conveying any rights to any intellectual
+//  property of any third parties.
+
+#include "assimp_parser.h"
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/matrix4x4.h>
+#include <assimp/postprocess.h>
+
+namespace vox {
+bool AssimpParser::load_model() {
+    Assimp::Importer import;
+
+    return true;
+}
+
+}
