943a160edaca42bb8e76c7348e87761badda0723
yangfengzzz <yangfengzzz@hotmail.com>
2022-05-27 14:17:56 +0800

    add pbd

---
+++ vox.force/math_functions.h
@@ -0,0 +1,44 @@
+//  Copyright (c) 2022 Feng Yang
+//
+//  I am making my contributions/submissions to this project solely in my
+//  personal capacity and am not conveying any rights to any intellectual
+//  property of any third parties.
+
+#pragma once
+
+#include "vox.force/common.h"
+
+// ------------------------------------------------------------------------------------
+namespace vox::force {
+class MathFunctions {
+private:
+    static void jacobiRotate(Matrix3r &A, Matrix3r &R, int p, int q);
+
+public:
+    static Real infNorm(const Matrix3r &A);
+    static Real oneNorm(const Matrix3r &A);
+
+    static void eigenDecomposition(const Matrix3r &A, Matrix3r &eigenVecs, Vector3r &eigenVals);
+
+    static void polarDecomposition(const Matrix3r &A, Matrix3r &R, Matrix3r &U, Matrix3r &D);
+
+    static void polarDecompositionStable(const Matrix3r &M, const Real tolerance, Matrix3r &R);
+
+    static void svdWithInversionHandling(const Matrix3r &A, Vector3r &sigma, Matrix3r &U, Matrix3r &VT);
+
+    static Real cotTheta(const Vector3r &v, const Vector3r &w);
+
+    /** Computes the cross product matrix of a vector.
+     * @param  v		input vector
+     * @param  v_hat	resulting cross product matrix
+     */
+    static void crossProductMatrix(const Vector3r &v, Matrix3r &v_hat);
+
+    /** Implementation of the paper: \n
+     * Matthias Muller, Jan Bender, Nuttapong Chentanez and Miles Macklin,
+     * "A Robust Method to Extract the Rotational Part of Deformations",
+     * ACM SIGGRAPH Motion in Games, 2016
+     */
+    static void extractRotation(const Matrix3r &A, Quaternionr &q, const unsigned int maxIter);
+};
+}  // namespace vox::force
