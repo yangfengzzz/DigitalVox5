5be879762ef529c1c98e5d608997b9f32460f457
yangfengzzz <yangfengzzz@hotmail.com>
2022-05-27 11:59:16 +0800

    add discregrid

---
+++ vox.force/discregrid/timing.cpp
@@ -0,0 +1,18 @@
+//  Copyright (c) 2022 Feng Yang
+//
+//  I am making my contributions/submissions to this project solely in my
+//  personal capacity and am not conveying any rights to any intellectual
+//  property of any third parties.
+
+#include "vox.force/discregrid/timing.h"
+
+namespace vox::force::discregrid {
+
+std::unordered_map<int, AverageTime> Timing::m_averageTimes;
+std::stack<TimingHelper> Timing::m_timingStack;
+bool Timing::m_dontPrintTimes = false;
+unsigned int Timing::m_startCounter = 0;
+unsigned int Timing::m_stopCounter = 0;
+unsigned int Timing::m_id_counter = 0;
+
+}  // namespace vox::force::discregrid
